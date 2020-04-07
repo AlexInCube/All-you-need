4ada4eef2397688c13280337c3854fe78d612b37
AlexInCube <mr.tsimbalistiy@gmail.com>
2020-04-07 08:09:32 +0300

    Version 0.3
    Added  functionality to super_tool

--- src/main/java/com/alexincube/allyouneed/items/super_tool.java
+++ src/main/java/com/alexincube/allyouneed/items/super_tool.java
@@ -1,12 +1,93 @@
 package com.alexincube.allyouneed.items;
 
-import com.alexincube.allyouneed.setup.ModItemGroups;
-import net.minecraft.item.Item;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.RotatedPillarBlock;
+import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.IItemTier;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.item.ToolItem;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.common.ToolType;
 
-public class super_tool extends Item {
-    public super_tool(){
-        super(new Item.Properties()
-                .maxStackSize(1)
-                .group(ModItemGroups.MOD_ITEM_GROUP));
+import java.util.Map;
+import java.util.Set;
+
+public class super_tool extends ToolItem {
+    private static final Set<Block> EFFECTIVE_ON = ImmutableSet.of();
+    protected static final Map<Block, BlockState> HOE_LOOKUP = Maps.newHashMap(ImmutableMap.of(Blocks.GRASS_BLOCK, Blocks.FARMLAND.getDefaultState(), Blocks.GRASS_PATH, Blocks.FARMLAND.getDefaultState(), Blocks.DIRT, Blocks.FARMLAND.getDefaultState(), Blocks.COARSE_DIRT, Blocks.DIRT.getDefaultState()));
+    protected static final Map<Block, Block> BLOCK_STRIPPING_MAP = (new ImmutableMap.Builder<Block, Block>()).put(Blocks.OAK_WOOD, Blocks.STRIPPED_OAK_WOOD).put(Blocks.OAK_LOG, Blocks.STRIPPED_OAK_LOG).put(Blocks.DARK_OAK_WOOD, Blocks.STRIPPED_DARK_OAK_WOOD).put(Blocks.DARK_OAK_LOG, Blocks.STRIPPED_DARK_OAK_LOG).put(Blocks.ACACIA_WOOD, Blocks.STRIPPED_ACACIA_WOOD).put(Blocks.ACACIA_LOG, Blocks.STRIPPED_ACACIA_LOG).put(Blocks.BIRCH_WOOD, Blocks.STRIPPED_BIRCH_WOOD).put(Blocks.BIRCH_LOG, Blocks.STRIPPED_BIRCH_LOG).put(Blocks.JUNGLE_WOOD, Blocks.STRIPPED_JUNGLE_WOOD).put(Blocks.JUNGLE_LOG, Blocks.STRIPPED_JUNGLE_LOG).put(Blocks.SPRUCE_WOOD, Blocks.STRIPPED_SPRUCE_WOOD).put(Blocks.SPRUCE_LOG, Blocks.STRIPPED_SPRUCE_LOG).build();
+    public super_tool(IItemTier tier, int attackDamageIn, float attackSpeedIn, Properties builder) {
+        super((float)attackDamageIn, attackSpeedIn, tier, EFFECTIVE_ON, builder.addToolType(net.minecraftforge.common.ToolType.PICKAXE, tier.getHarvestLevel()).addToolType(ToolType.AXE, tier.getHarvestLevel()).addToolType(ToolType.SHOVEL, tier.getHarvestLevel()));
+
+    }
+
+    public ActionResultType onItemUse(ItemUseContext context) {
+        World world = context.getWorld();
+        BlockPos blockpos = context.getPos();
+        int hook = net.minecraftforge.event.ForgeEventFactory.onHoeUse(context);
+        if (hook != 0) return hook > 0 ? ActionResultType.SUCCESS : ActionResultType.FAIL;
+        if (context.getFace() != Direction.DOWN && world.isAirBlock(blockpos.up())) {
+            BlockState blockstate = HOE_LOOKUP.get(world.getBlockState(blockpos).getBlock());
+            if (blockstate != null) {
+                PlayerEntity playerentity = context.getPlayer();
+                world.playSound(playerentity, blockpos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+                if (!world.isRemote) {
+                    world.setBlockState(blockpos, blockstate, 11);
+                    if (playerentity != null) {
+                        context.getItem().damageItem(1, playerentity, (p_220043_1_) -> {
+                            p_220043_1_.sendBreakAnimation(context.getHand());
+                        });
+                    }
+                }
+
+                return ActionResultType.SUCCESS;
+            }
+        }
+        BlockState blockstate = world.getBlockState(blockpos);
+        Block block = BLOCK_STRIPPING_MAP.get(blockstate.getBlock());
+        if (block != null) {
+            PlayerEntity playerentity = context.getPlayer();
+            world.playSound(playerentity, blockpos, SoundEvents.ITEM_AXE_STRIP, SoundCategory.BLOCKS, 1.0F, 1.0F);
+            if (!world.isRemote) {
+                world.setBlockState(blockpos, block.getDefaultState().with(RotatedPillarBlock.AXIS, blockstate.get(RotatedPillarBlock.AXIS)), 11);
+                if (playerentity != null) {
+                    context.getItem().damageItem(1, playerentity, (p_220040_1_) -> {
+                        p_220040_1_.sendBreakAnimation(context.getHand());
+                    });
+                }
+            }
+
+            return ActionResultType.SUCCESS;
+        }
+
+
+        return ActionResultType.PASS;
+    }
+
+    public boolean canHarvestBlock(BlockState blockIn) {
+        Block block = blockIn.getBlock();
+        int i = this.getTier().getHarvestLevel();
+        if (blockIn.getHarvestTool() == net.minecraftforge.common.ToolType.PICKAXE) {
+            return i >= blockIn.getHarvestLevel();
+        }
+        Material material = blockIn.getMaterial();
+        return material == Material.ROCK || material == Material.IRON || material == Material.ANVIL;
+    }
+
+    public float getDestroySpeed(ItemStack stack, BlockState state) {
+        Material material = state.getMaterial();
+        return material != Material.IRON && material != Material.ANVIL && material != Material.ROCK ? super.getDestroySpeed(stack, state) : this.efficiency;
     }
 }
--- src/main/java/com/alexincube/allyouneed/setup/ModItems.java
+++ src/main/java/com/alexincube/allyouneed/setup/ModItems.java
@@ -4,6 +4,7 @@ import com.alexincube.allyouneed.allyouneed;
 import com.alexincube.allyouneed.items.portable_nether_portal;
 import com.alexincube.allyouneed.items.super_tool;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemTier;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -14,8 +15,9 @@ public final class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, allyouneed.MODID);
 
     // This is a very simple Item. It has no special properties except for being on our creative tab.
-    public static final RegistryObject<Item> SUPER_TOOL = ITEMS.register("super_tool", () -> new super_tool());
+    public static final RegistryObject<Item> SUPER_TOOL = ITEMS.register("super_tool", () -> new super_tool(ItemTier.DIAMOND,5,-2,new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)));
     public static final RegistryObject<Item> PORTABLE_NETHER_PORTAL = ITEMS.register("portable_nether_portal", () -> new portable_nether_portal());
+
 
 
 }
